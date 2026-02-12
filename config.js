19a734c70a4c4be920f43b8ac8bab822c79f816f
natividadepedro0404-sudo <natividadepedro0404@gmail.com>
2026-02-12 17:40:35 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,9 @@
 // Altere esta URL para o endereço do seu servidor backend
 const CONFIG = {
     // Para desenvolvimento local:
-    BACKEND_URL: 'https://9ffc-2804-1b3-9103-f14b-c13c-f3b6-f032-ca7c.ngrok-free.app',
+    BACKEND_URL: 'https://backend-1tko.onrender.com',
 
     // Para produção (use ngrok ou similar):
     // BACKEND_URL: 'https://seu-ngrok-url.ngrok.io',
 };
+
